7f0563d4d6cae4f249e15fc96ae987d9ad4e9d45
Владимир <100162794+Vladimir-Korolev555@users.noreply.github.com>
2023-10-30 21:45:08 +1100

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+
+public class Main {
+    public static void main(String[] args) {
+        String test = "Мне попалась очень сложная задача!";
+        char[] testSymbols = test.toCharArray();
+        StringBuilder sb1 = new StringBuilder();
+        StringBuilder sb2 = new StringBuilder();
+        for (int i = 0; i < testSymbols.length; i++) {
+            if (i < testSymbols.length / 2) {
+                sb1.append(testSymbols[i]);
+            } else {
+                sb2.append(testSymbols[i]);
+            }
+        }
+        System.out.println("Первая половина: " + sb1.toString());
+        System.out.println("Первая половина: " + sb2.toString());
+
+    }
+
+}
